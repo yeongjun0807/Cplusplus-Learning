c12f362bd4c5299d02208d238e758c0467051615
Programmer <62930087+yeongjun0807@users.noreply.github.com>
2023-01-08 15:33:48 +0900

    upload swap.cpp

---
+++ Source/C++ Source.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+
+	return;
+}
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	swap(a, b);
+
+	cout << "a : " << a << " b : " << b << endl;
+}
