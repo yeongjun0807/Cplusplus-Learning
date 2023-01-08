e8cc14b919ac6f9ca131b585fa5c9ea6623e494d
Programmer <62930087+yeongjun0807@users.noreply.github.com>
2023-01-08 15:52:16 +0900

    Create chapter1-1.cpp

---
+++ Source/Exercise/chapter1-1.cpp
@@ -0,0 +1,19 @@
+// exercise
+// Chapter1-1
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    string name;
+
+    cout << "input your age and name : ";
+    cin >> age >> name;
+    cout << "Your age is " << age << " and your name is " << name << endl;
+     
+    return 0;
+}
